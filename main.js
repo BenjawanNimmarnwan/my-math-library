f522399a635584659fdcac2c23850e84df0cca80
Benben <benjydecor@gmail.com>
2021-02-09 12:01:43 +0700

    refactor

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 function sum(a,b){
-    return a+b
+    if(Array.isArray(a)){
+        return a.reduce((total,n)=>total +n,0)
+       
+    }  
+     return a+b
+
 }
+
 
 module.exports ={
     sum,
